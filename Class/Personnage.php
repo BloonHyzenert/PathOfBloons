9e4e3489abbc6a10428cfe1560882099b44634c4
erwanchery <chery.erwan@laposte.net>
2020-03-24 12:23:56 +0100

    Fix: setter

--- Class/Personnage.php
+++ Class/Personnage.php
@@ -5,15 +5,7 @@ abstract class Personnage {
   protected $str_nom;
   protected $int_pv;
   protected $int_attaque;
-
-  public function subit($int_damage){
-    $this->int_pv -= $int_damage;
-  }
-
-  public function soigne($int_heal){
-    $this->int_pv += $int_heal;
-  }
-  
+ 
   public function get_nom(){
     return $this->str_nom;
   }
@@ -28,5 +20,9 @@ abstract class Personnage {
   
   public function set_attaque($int_attaque){
     $this->int_attaque = $int_attaque;
+  }
+
+  public function set_pv($int_pv) {
+    $this->$int_pv = $int_pv;
   }
 }
