6a20d92e8fa178850d4dda0483b47754e0d4f38f
BloonHyzenert <audran1803.ddc@gmail.com>
2020-03-24 17:27:44 +0100

    ammend

--- Class/Monstre.php
+++ Class/Monstre.php
@@ -4,13 +4,14 @@ require_once 'Personnage.php';
 
 class Monstre extends Personnage implements \JsonSerializable {
 
-  public function __construct($str_nom, $int_pv, $int_attaque, $int_defense, $int_esquive, $int_critique) {
+  public function __construct($str_nom, $int_pv, $int_attaque, $int_defense, $int_esquive, $int_critique, $str_image) {
     $this->str_nom = $str_nom;
     $this->int_pv = $int_pv;
     $this->int_attaque = $int_attaque;
     $this->int_defense = $int_defense;
     $this->int_esquive = $int_esquive;
     $this->int_critique = $int_critique;
+    $this->str_image = $str_image;
 
   }
 }
