f2cd07e58b123d585c03610085d1a29ab10159dd
BloonHyzenert <audran1803.ddc@gmail.com>
2020-03-25 11:01:44 +0100

    Image + Stats

--- index.php
+++ index.php
@@ -4,12 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/healthBar.css">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <title>Path of Bloons</title>
 </head>
 
 <body>
     <h1>Path of Bloons</h1>
+        <div id="hero">
+            <div class="container">
+            <div class="col-md-12">
+                <div class="health-box">
+                    <div class="health-bar-red"></div>
+                    <div class="health-bar-blue"></div>
+                    <div class="health-bar"></div>
+                    <div class="health-bar-text"></div>
+                </div>
+            </div>
+            </div>
+            <div id="stat">
+                <div>
+                    <img class="icon" src="./ressources/epee.png"></img>
+                    <p>12</p>
+                </div>
+                <div>
+                    <img class="icon" src="./ressources/critique.png"></img>
+                    <p>100%</p>
+                </div>
+                <div>
+                    <img class="icon" src="./ressources/bouclier.png"></img>
+                    <p>5</p>
+                </div>
+                <div>
+                    <img class="icon" src="./ressources/esquive.png"></img>
+                    <p>30</p>
+                </div>
+            </div>
+        </div>
 
     <div id="plateau_jeu">
         <p id="titre">Bienvenu dans notre super jeu. Veuillez choisir parmit les 3 personnages :</p>
@@ -19,13 +50,14 @@
         <img class="image" src="./ressources/pretre.jpg"></img>
     </div>
     <form id="choose_path" type="POST" action="#">
-        <button type="button" value="0">Guerrier</button>
-        <button type="button" value="1">Mage</button>
-        <button type="button" value="2">Prêtre</button>
+        <button class="blur" type="button" value="0">Guerrier</button>
+        <button class="blur" type="button" value="1">Mage</button>
+        <button class="blur" type="button" value="2">Prêtre</button>
     </form>
     </div>
 
 
     <script src="js/index.js"></script>
+    <script type="text/javascript" src="js/healthBar.js"></script>
 </body>
 </html>
