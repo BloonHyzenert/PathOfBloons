a41c1339bba5bc25d7b63e2c9fe2e76994d62be0
erwanchery <chery.erwan@laposte.net>
2020-03-26 14:54:08 +0100

    Cooldown sort guerrier

--- Class/Hero.php
+++ Class/Hero.php
@@ -9,8 +9,8 @@ abstract class Hero extends Personnage {
   protected $int_niveau;
   protected $arr_sorts;
   
-  public function learn_sort($str_nom, $int_degat, $str_effet, $str_image) {
-    $this->arr_sorts[] = new Sort($str_nom, $int_degat, $str_effet, $str_image);
+  public function learn_sort($str_nom, $int_degat, $str_effet, $str_image, $int_cooldown = 0, $int_cd_done = 0) {
+    $this->arr_sorts[] = new Sort($str_nom, $int_degat, $str_effet, $str_image, $int_cooldown, $int_cd_done);
   }
 
   public function nouveau_niveau() {
